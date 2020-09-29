7f704ec5156a868b0ac32bcf69088c910fa0e1c6
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-28 19:40:05 -0700

    add routes to handle admin view and role update

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users, controllers: { registrations: 'registrations' }
 
+  get '/users/admin', to: 'users#admin'
+  patch '/roles/update', to: 'roles#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
 end
